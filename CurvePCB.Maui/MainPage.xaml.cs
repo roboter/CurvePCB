d2a543c7d54eaf62a88d52ff20ada295c2a7d113
aleksei <robby.roboter@gmail.com>
2023-01-03 00:35:10 +0200

    Maui app

---
+++ CurvePCB.Maui/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace CurvePCB.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void MainGraphicsView_DragInteraction(System.Object sender, Microsoft.Maui.Controls.TouchEventArgs e)
+        {
+            this.drawable.Move(e.Touches.FirstOrDefault());
+            this.MainGraphicsView.Invalidate();
+        }
+    }
+
+    public class MyGraphicsDrawable : IDrawable
+
+    {
+        Point x;
+        public void Move(Point x)
+        {
+            this.x = x;
+        }
+
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            canvas.StrokeColor = Colors.Red;
+            canvas.StrokeSize = 6;
+            canvas.DrawLine(new Point(0, 0), x);
+        }
+    }
+}
+
---
+++ CurvePCB.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CurvePCB.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+

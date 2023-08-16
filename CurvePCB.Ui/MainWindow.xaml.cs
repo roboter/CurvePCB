eca01aaaa42ad4b14ac674b2496773c43d2b752f
Aleksei Prokopov <roboter@begemotik.ee>
2023-08-16 23:25:20 +0300

    UI

--- CurvePCB.Ui/MainWindow.xaml.cs
+++ CurvePCB.Ui/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.VisualBasic;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
